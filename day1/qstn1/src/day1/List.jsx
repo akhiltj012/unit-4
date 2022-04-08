4cfca62b668d183ad8ff8fcd05e872e04648ab67
Akhil TJ <getakhil012@gmail.com>
2022-04-08 18:57:24 +0530

    src

---
+++ day1/qstn1/src/day1/List.jsx
@@ -0,0 +1,20 @@
+const Head = ()=>{
+    return <div> 
+        <h1> Mobile Operating System</h1>
+        <ul>
+            <li>Android</li>
+            <li>Iphone</li>
+            <li>Blackberry</li>
+            <li>Windows Phone</li>
+        </ul>
+        <h1>Mobile Manufactures</h1>
+        <ul>
+            <li>Samsung</li>
+            <li>HTC</li>
+            <li>Micromax</li>
+            <li>Apple</li>
+        </ul>
+    </div>
+}
+
+export default Head
